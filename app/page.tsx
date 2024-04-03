35282d1565ee78cd6181625b39b77ff390aeb0b7
drili <DriliB@hotmail.com>
2024-04-03 15:40:19 +0200

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1>NextJS Tracking Tracker</h1>
+		<main className="flex flex-col items-center gap-10 justify-center py-10">
+			<span>
+				<h1 className="text-5xl font-normal">NextJS Tracking Tracker</h1>
+			</span>
+
+			<span>
+				<h2>Table of clients</h2>
+			</span>
 		</main>
 	);
 }
