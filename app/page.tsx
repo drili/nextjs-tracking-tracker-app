1064bf54e390ec6ef34400b10fb2f02a9bad13a5
drili <DriliB@hotmail.com>
2024-04-18 14:47:41 +0200

    :construction: wip settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
 import Image from "next/image";
+import type { NextPage } from "next";
 
-export default function Home() {
+import settings from "./lib/settings_data/settings_data.json"
+
+import { SettingsData } from "./types/settings/SettingsData";
+
+const Home: NextPage = () => {
+	const { settings_frontpage } = settings as SettingsData
+
 	return (
 		<main className="flex flex-col items-center gap-10 justify-center py-10">
 			<span>
-				<h1 className="text-5xl font-bold uppercase">NextJS Tracking Tracker</h1>
+				<h1 className="text-5xl font-bold uppercase">{settings_frontpage.page_heading}</h1>
 			</span>
 
 			<span>
-				<h2 className="">Table of clients </h2>
+				<h2 className="">{settings_frontpage.page_subheading}</h2>
 			</span>
 		</main>
 	);
 }
+
+
+export default Home
---
+++ app/types/settings/SettingsData.ts
@@ -0,0 +1,6 @@
+export interface SettingsData {
+    settings_frontpage: {
+        page_heading: string;
+        page_subheading: string;
+    }
+}
